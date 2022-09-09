0fe4984bbe61321d11bb8bd64f57c821f1168ee3
sagge14 <108992042+sagge14@users.noreply.github.com>
2022-09-09 22:53:15 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+using namespace std;
+
+class B
+{
+    int x;
+public:
+    B(int n) : x(n){};
+    void print() const {cout << "x = " << x << endl;}
+};
+class A
+{
+    vector<B> mB;
+public:
+    A(initializer_list<B> list): mB(list) {};
+    auto begin(){return this->mB.begin();}
+    auto end(){return this->mB.end();}
+};
+
+int main() {
+
+    cout << "---Initializer_list/for_auto---" << endl;
+    auto A1 = A{1,2,3,4,5};
+
+    for(auto b:A1)
+        b.print();
+
+    cout << "---Bye, bye!---" << endl;
+    return 0;
+}
